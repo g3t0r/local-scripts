50ad1d84e04cf78b678e82a60c07360581c5f59d
Jan Jakubowski <jan.jakubowski.ti@gmail.com>
2024-12-12 01:49:04 +0100

    wifi signal strength

--- sb-net.sh
+++ sb-net.sh
@@ -1,2 +1,4 @@
 interfaces=$(ip link | grep "state UP" | awk -e '{print $2}' | sed -e "s/://")
-echo $interfaces
+wifi_sig="$(nmcli --colors no -f IN-USE,SIGNAL device wifi list --rescan no | grep '*' | awk '{print $2}')";
+echo "$interfaces $wifi_sig"
+
